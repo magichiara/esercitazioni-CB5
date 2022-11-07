af30f0c70334a41cc15463b33f7bd0e84fcbc12d
magic_c <info.chiaramaggio@gmail.com>
2022-11-07 18:22:29 +0100

    esercitazione 03-11-22 - FINAL

---
+++ 03-11-22 - FINAL/projects.js
@@ -0,0 +1,115 @@
+const projects = [
+    {
+      id: 1,
+      title: "Glutenfree",
+      category: "UX/UI Design",
+      year: 2017,
+      image:
+        "https://lh3.googleusercontent.com/T39Raj-ynZDF5hGbY7F1X8abc88wjCShztA2l6iucrBHnbZtCL_-HwjwZAQvV79FWohK4sq5nIrVQyFh7bXFUXztl9BA7dDuXSWMFeeFFC8ZvqhNtt6yv7K50uoC2wcKsSeuTrO6BQCVmH3PhSVfqkUVjn5qiWmaq6GoLrgvZk93kajrgUqvc8K-UIAIfXByHYtyFqLSMJJZ08HWG0yiQJvPJ-IUJYv1057NPyXQlRa7291MH8iwreHxTrVcOcbIoGPLV3unuDRwxcktFBgS9nl_yk3ngGYo-zVvnpSZ-UL4paQYLDwrIUdIK0ejUGpoYu2rJ0oR9mIsjXBlFuLJb541gD9pDxvOdYfV-CTouL4bIdFn0NCRIhCWBLm-_4Z8uRPrOSCa1wxKidJfiDiSMVQVceTtvhSF6irJ-PqGMBNjo25Ek14lTgEPRTTclsQy3Qs3eFt-Ozdskw7T_FUQdYIB63SB_PRhEtUBvqbdMnpxZtavnvwmg9GVlE0W0SojI2AClXgnsyl0IB6Tvf2dbc8hgfBe7_X4YizJoEmIFD2jC2WbRW-vN1FBDvMwoFhKDRMUShB6IJE5rS16Fvw7GHiQroMPCvii8OX32oI77vBNRhd9P1PEBnxVnbBbgHX0WWib6vd9e5luWHah3ohnv4-mBjhTiYJ9imCDeNu944Y5kDOp2dQOWtVOLh07t8N8gnIo_2zmGOLw1jJnjdv2C4u1tkVN09YEQyDsOS9KVnrQYA4jYdNOGJIFmpUIzf8TVl-Us1l2MQhDClOfiVtAReWTGJTQ3pJM8n2kycK5_nt5cOwxsoVd_OW8F7qL5V0GcZ0JbwfX70dt6uv2YMbj6PuoLvXLIdsNN2y1OKYEVde_FAFxCsNHkcBaXnIKSgwjRBMSMSydRJEJDGUJ2jmDb6ZVxgZLEKWdlNZpFgXpeC1nVG8qL6eu_H0vY20pv8iyzGY2FxizJZWErczlIESofVirgNr5I9tp7Rl94Y7cCqDzv8Sr-kWNbmWQ9cOmG7BFN23nAkNjF-N7arZoj55CuiC_YQ=w263-h329-no?authuser=1",
+      isComplete: true,
+      isInTeam: false,
+    },
+    {
+      id: 2,
+      title: "Recycler",
+      category: "UX/UI Design",
+      year: 2021,
+      image:
+        "https://lh3.googleusercontent.com/uFK5eplTAlv2ntBlTKvMntTmzrwbvqbAVsZXD_sobvULZmFavVdJdeAAQM8nTdC_SYJFTISY8bmhj0GPg0f0r-XvoolW7N-DFuxCchW5PNzuu5QJat_NBGdnDFLg6UaYeQtUs-BGimsm84He5BpEU8WLzqR1ZVTSYEK18PV5ttZuTgbjl8EVMuCr4D21WRVEPm0lWV6RuM9cuJnhOR1NQIkcjbsqpMIx0qZRqtrf1vOaJwz_SEHGv81Il3h7ZLBr74NBlk8byNI2ocuVQdSANZT415zJTgxTydf60fsIFTeZ7Zt8Q6oicTxhd7_BgSdAXpi4HymzlMVpm6dXCmDT0yW0ivn2xCwYr7-KwBq8C3wRzGdmTWAszytJc4U_1Nr27Bka1uV4G4eVoNLOZFUhO9zwvi5Xi9KMLO3oCBOaTT9eeGdvO7BSszsilIP2IpNRU-3vqJ99wGqsP-NwMyRMHjD_eJd9y2uHxzbsHwG8iNLj9OzZNPxQcRbN_WlyoBpqujNawV0189rnUBAUDCoUDByN5dHqNblU6yJmPuYQuf2jDwNiKZQCoWs0yQ-5L4wukk4qVRG2ZG2iFtuGPdgXnZw3v2jSLtfolTmE_mAQDqyIgGe6lk8POuukuG0iEtaLnnyj4W930AX-moRxXWmkOsPAScPaq0IKx18eEJs8GfL3pgUz7b54dMXVEuZR-XsUHFEH-8iDs4omYpI9ANw8hiamEzjJ8b8lzpI3ilDeivg4tHkdgL_1LTDQGQI5qICjaNHqIG4UYNpkRLWM5qBrfwobVTpiGtybur9VViEz3D73kMxnG-QlZo9XE6Vkf-oQEp2Sj6UA5sKWTSi5z5NY1bUhciQy3m_hDDq6uWbMfxjMCBu7DXBQRSrmDsb7n_mcXoEjz9IUQGSbl7bMondhXRILz8-9oCFHsZcEBoc2xGsER_E6zqhytDAjRJkETkGA91zcZJKpIePk7fpZVUNBUD4YN1p2fbnzUzFxaIVPo-gkWwOWglqtgx6b54YRf7Td-P9lQZ4JclhnUDyoSG1iiQIzqQ=w218-h308-no?authuser=1",
+      isComplete: true,
+      isInTeam: false,
+    },
+    {
+      id: 3,
+      title: "Homepal",
+      category: "UX/UI Design",
+      year: 2021,
+      image:
+        "https://lh3.googleusercontent.com/WS-3eweh4y6OOBGBE8nEH4DEMBRQkrAspQIbW-0ODW9Q9Im667jKOjALe6m5KkN9Lnu7m-hW4-tLbYWfo-TBeRdMtgWyrk4KnGuQHGXKuzphxCnQsVEryTwSrBrUdA9UYJ_5-RU67YdS9kHQHjVGRbyibLddTvYFi5ZUk_1nmMwplQpYo1M3b9qt27hBOSOPEhELY-K61NaTyYrqmJdZvC3IVY-J0M47IhQV7NpjVATR-SSY9R39OT-xD_S796qzFdz-A26ckFsuyhs3ZBXTn9fq8nXsK5_8i1jhobvOHhWdM0x7o7UMQZzBGVMCM4kulFQjS786ukjtBhcYm8HPrpQsknIGhMM-hnTMMYQVmOnexQgl5MqGIdAESwkp5zxInmFqvBXHHSrKJz1-xkIDahxGffoYi4gCDZ9PXWb6VtLVgzK9Ug5EXffZNTCevxVZ8-EzdNE7hqxcSm3-bqGnLpAc_2lbzsMMlf0IH49toY5toKY4BMhhP3zUhxeaP8ubmAUPPgWAIPMKXbtKIAp2ew3ekwnuUwGpPVNAIvyZ5OV0FVmcir6YSUTWqYINfSKPa42WlOjPpuMPt3Sy0hxTDyjdIXrdn4s378CDwGtJcbAIceqeQpVc5-8H5KV-HQ7RR5Pw2bMLw51ebsfAqrNaLU2pZ4dPGy_wEAyGqpefXhTldeOFMt9OoNbjSpd9Vz7-s3qgyj114LLOWH_a5Dk2qlJXyPsRXsE5wPYs16CvluQdbGSSTUcEHUCZrENIQd-SJ5aPdG1F04GmjaQgkgnDcerzGGYp8jyMu8tyTQWEPViMD0SjvkF3k62Tjlx_ypwslsvi0YI4PPkc6olKuc7FjH6bPqN8fe_t4DBvftbIVAXQ8Eh5mogJObDqi_29K-Et9fNOJQ8NypjfiuVl3-A6oEWH5DQZou3UxBTYCJ5vJwwBZUciKRPhp1t3B8zO5ulenoPurqCeF2sgRQCn2JURpCbgHPcaON83-zJDMNgCG2Wt9v5HfatoLufS8YyWt5KE4iTAHrUP_xfqG_Pi8eoEolDGhw=w594-h648-no?authuser=1",
+      isComplete: false,
+      isInTeam: false,
+    },
+    {
+      id: 4,
+      title: "Happy Hour",
+      category: "Packaging Design",
+      year: 2020,
+      image:
+        "https://lh3.googleusercontent.com/cJa28EtAXuRNpGQZfBgZUURScTYU-Sd9RBr013YAbfk5l7pWkG77FLnzSnMw1Z_NubGzgEkey5aVywN0p2rTDHWf6nNY4IEhQkvCRYuvpAoKpqMOt3kaZ-NS0QIKME8KJytPCBlvNuHT3RIFfrqOl1uvTGjDjrwrBosxP73tkcCS-c2W3TUm6mIpXmQ2UAztsr_HQZ-N7tVzKNsViVya0ar7eEn0QQD0Z_Gxx53GoWNW_8-A8HAfZNWvEPgyIyxjI747Z4_amkfdOBGPaAgKgA3TTZBLS9gGZfHhayfcisJjh5kQ6TAywsuDRRo4LPiYz2ELDzh-m5YY-C2LkWGsmGp6eVKxVos8SuiBAilXrq1LU8j-KHDEEkMIg-MI_iySdmCIr-R08EjcSrJvINe-LqercGSAEah0oEhXdV5Ban-kJ78B6fa1KODUhbDSZhYRG0ZSDKK9Z-EeZ-1ySM3ZhBm_DU4P4RbHZPf7cBDMUTB_slR7niam-9cDHDSbKhl8l0KGwFyMYSpX6yvscqpJynzeVMTYIWN7MOKuMXQICrI1Bh1roKjKpLKaJWpmUJsu12X8pEussl_kbYJhA6lgeC6QTQAfIzVj72PKimnD7zHyA5xc-FU9aqCazO3AUBIUpANNHW-KbMz3N-YSLj2RQRS94Fzz7LyYqw6thNXiIpIifxEHlGEWTI9442B1wS9qzbuetcc_zfdiPJvrfm-FCDQgQ2Cfjih8N8zHyOae85BkHYQlK9eVr_L2ngx6n0YG3pcEt8SfXQ6HDTYibALS0mVXKCPoyDqyMWp6GkdLaG-g3jxY9j4fFexmM57jiqHwFsPi-LXmNdgBHeL6RRCFCPSi4oTDiXYfjHVZl1PuO54O8LQnfZXRXll3CefVIMhvobogwMradIviOe9URJE986nFd7gXRNETErjv6SSonSZQEyxdpPQWTSRmiXqZtR6GO5pIbUBbPm7P02WB1HzQj1vT_dt7eh_7eysBPVVkvS0PzicNZ77dfhZQGvRgtGfkxhWWURwQ7THOOLRucavLqSbfXg=w418-h334-no?authuser=1",
+      isComplete: true,
+      isInTeam: false,
+    },
+    {
+      id: 5,
+      title: "FILA Packaging Award",
+      category: "Packaging Design",
+      year: 2020,
+      image:
+        "https://lh3.googleusercontent.com/aYTNr0vgR8QqBSTSkzF2BpOgx20DgTPFjZhaijNrSvl5g_lOQANzN1i7mxQq0ERNvLZkJw4OB1X1evgDga4xuDX2HEOxkTpvjuDxU3wjnGiPbvlK-ZMVCrkMfiT8dgLbgXnqK7tS3gpkhI9u4T6qZIfPyOka8ndX3PRF4L4o1Jp1OO6G8nCcMnZZ90EZmuEbXciTfic7LpczdOHqspGPgYEViBUZODUOpZc9Oa5kZb-GAIjSSD67Ph1j4sdfagdLnRs8oF7RmcWla8LBjXryYmI-yiTjnMSZ1s6-sKXd5JNm56XhxtpmLJRLPNZrO3sIQSmmdFVYsMAtnS6pBxNhvRR362M14xoMlOdRb84mtyxwjNCtMwKHt2muiqSuRs6w0M7Etm-Yjg4G9b3wssOC2f8FA6DBEp1XLHjQW5ScgEfDX5odXEDJSc80itOrzNutciIecLYUUSgYLycbf5MOjdfmrs5_A4Gsw6QCBdq8ZVIz2sWs_wfsf_uNvyHZ91n-uym22rdyEJ5OHlIVi2aOl0Txc0umrL16dFksq43tDD4ZVnUVcgG64SfY8CGjmVo9sONY4ID7BRWT0NJ3ERiCbvTwWQ-T1mGymZgLA4FjOi4uACjGEZdR4TQh_k9jEs0nnzWJ2XsSM4_5MjVyilNBI3jOZDh_KOi15R8vF5sJB-gQrURxPUnBdnSQ3rzqoRwRN9_WUGA6AxKZd8fkpuOPgSIt1z3x0SsxqEhE6b_w8TbpLuWFmb37MJ6rVldSAsUOZIWKXoW_iRYw7ZZySMSPErO_D6ioi2lHKLhS0rILf-YXKF5RgkvzthyKsqWmwbiYesXv4bYdJEvwav5H_s4kWV-jOe6N8oQvy0lMHVt6pdL39AW__NQLnioGyzYmc3FYw0pGOVoipIej_gyui5vmzt7g6ofaM5VY7_hd6JRcpCHD3cuZGqthHZwZyiY3dujdEenV53RCwkl9yX7DEFuYsVC_VkBtn-9vDiOeXsikjWs3V7NoLv4lEFlmRFOaazpBleGnqDiZVJ5qjjknCCR4e9GMNA=w1056-h1578-no?authuser=1",
+      isComplete: true,
+      isInTeam: true,
+    },
+    {
+      id: 6,
+      title: "Conservatorio A. Corelli",
+      category: "Brand Design",
+      year: 2020,
+      image:
+        "https://lh3.googleusercontent.com/f002XTwq72ZsXxBhWFKKP6TR9MId4VFtQQhyO-JxNF58J1pFs69s9dtYkijXIO-t3mOPggVNV4wa0sLTDvOJpCOWtgdNSFmcYIaFTVCTSkz_cepO-0FdXO-efsuYVH90ToFM9HC-gzi9Ylgw5JQF0BPxXPxOSQqHVztLVf9fbO45wUh_UOsG5YWAUMVdV2kVONKG3a9Z9S34pfbN4UJotfDutBkx6wyZNHtxv1nS2QwVG5sAjZylka4QYBoopuaxAiyt5fRsqpD-NMSReQp-omL5J-nJQiqaldmWxsaEbefPvuUavILxLddLcHHEiz3vv0Q1-lIn9KKmbtybRlWpjE2MDUzJxRlJ-ODSCfFwhfbJHWfXyibXxRwq-4QBhDGcY_lcaAUwaYxkyTUDCfAAxQz56nc72PU7XD8tdO0m-1gP-9dsRqivZWgzh1gCd8v-I8INGIHDZm8P1P-vza_vcAmpzb-8QMkmPph9FP_rClLiwa0NjX_9Mpss16MMXbR5WTPRF3VrXe6cHufngG00yYgRW8959POR-h2drZE5y-s8ws2pOUipa_x_MZFE_jXqykOGbF2fXfsW5xBfS-XjRA4ddPmxddRL0-zjrPBzfKnQuPl4AswlWhhPUGi2v9qCoYb95aX33GxKb2XJqH8ktkjHnbzXR7NBPtuE3E0Xrc2yvpHpH5dCKxnEYC76Oqb3Le8Ll4d1SYVcKDU4zNQmOOFVI5OJUQBV8bV5CjMpzLpC_DK1cdb5bje5_AMZ3XAwrq_wJVKacnFLJTCP3upe9RqESfoUp1OnV7ZE-bvffxLmrD0xJ_pPp1GfUOHMVvRhyVCVJbt7Dvq1LNvs8a7ufzGnTmhzfdOetKJKBcmGRjp2MlxGOF7uUkFejcVri_A8mEYJZ-qBb1-NAMEYNtgPHJnushUrEefy7CRfkcNRBnRf2bXPe9rMPbYDZAnp2VqpUn49GquJtyPiXw2kX4NZWkZykbcmJ404CS5CQFnmUsd9G0DoqJenEAcdOtSM1QqpxQN62IaSzO8Tzj1G4_Kdf6VQtQ=s376-no?authuser=1",
+      isComplete: true,
+      isInTeam: false,
+    },
+    {
+      id: 7,
+      title: "Lo Studente in Crisi",
+      category: "Brand Design",
+      year: 2020,
+      image:
+        "https://lh3.googleusercontent.com/QKnU3D7erQj_SqDVyP3sph7zO_FOi2jf2v0fIBZpcS-vLXDEm3AyKMpqbJHNUhWrlfOyLRxmHBQdgndbBPsvG7lT0tvKfWIvSwa3uAhxag7GgOaX2dTzp4io6n2EJFjJdExnmImmYUcsJRKIplIx9Xei15LvKH10h4sh3tqHnLvlBCU1JLUeUreeKDxYXuEsj5WFN7pO7KqFQ7KuJSj36ZSsWP5JPtdwiNcLivDAk07BtN7qUAR86TLuBrJCA_Sf4hffNBaZ6JRh5BI2e3cVXHzzp33gJkzZq8AHZfnYbVwiPDqmvFeOIpL3j7lxXNeQimpbfA69fVd636s95UFS3de8dUls6oxsN46YzGeaDdExJ18grkdp33zdUMekt7NOuvMg8Ij83td33-LOH6uLXvWKxxHjD3d0KCm1zIIH-209X8h3gYu83m22yLSwvSZRbQ1DQ3zZ5unMzHurpB1Gn5W83KPoK2sRE0wxtzyRJDt-Q9TGdfWskTaES6yqDY7xw_k3v40YyndDBJUK1n6Bqpzw0j7nfXcZaqscx1ACP2-RKOo08jYsHqD-xhoghjwvhFLJegHV0DJvBEmjk-ALU5YziHOuay-orQZ5o2VhVYlkh5anCf_tKKOlOv8P0Y8xM6kdTSAgj_GFpZT1Ku19Yb-NEGyvsggBLJ2bid8AVl25hCvq28f2iEOJ1S0UUoO3Pvt-Zhjia87tlqM5iLI3nfuyNQrdjXGUgJgzFCBEVSndGKF8klB6iVS3Acn5BPUgYUgPKDRErGzH7U9Q4X6I25m8YhJGH191_DEMOMDaBwjP8DvE2P0otZ5YdojLXzGMm77D-GXkB3rc-Dkp3ZdT4vDUq6wQayp9lgoXtJob0qHPLndFbIILq0S6AMWfa0fLHwpUR3ar8ZT3iVJP_nt4yfVAzPty2bu2n9psRvfcEfoVUvahO4Y4dryQAQ5JgVETlqvmN66sdb0Kf5-yD9296_bNk9XCZYNCQTFTa9XS0k_-Zt9YEOwK6UqnEtFqz8w5RyVBydVQt9O5iJuMYM9XGCw2VQ=s1578-no?authuser=1",
+      isComplete: true,
+      isInTeam: false,
+    },
+    {
+      id: 8,
+      title: "Didot: type specimen",
+      category: "Layout Design",
+      year: 2019,
+      image:
+        "https://lh3.googleusercontent.com/HYoKhI03btKvK-4X6bOxMsJhTT_AeTlKFtb5rEy22xGeH2NzFa-0X-gBUYflUl24zHs_4KoU7pbeTK5u0T8-b6dJXHBB7FN26eXBXGX1rB0T5Xq2gEpSz910zbOa0GMkvQV_B5sQa6z6oJp3VR4ceauoYAu1RJ3vsMpn6F4DRGFoKCJmBOG3cN9IJua9FujJH0ODmoacAg_8CCCOKyE-KSf6FX6_vwIOrdVrqWeJLnnYakpETB6KPfKm-i6AlwCnXrF6OSz8vZ3KlNZK8ZftpcvzOW7cYV9JoUnc2lL_JKMk8raTOn62bblNFmsPyrPW7r9Yd17vezDnZSPLzPzy77G42iOsw__b27bDMU7CwAVR4nacocQZsKZNbckUpeZVQIeNCB2eYuVyss0FpXs7gvw7ZKgbot5wuICkDxNqvCSoaC89BXnIjupaSXUyDdLB_yV5pbPk1H5yGIlQTX0ufLfIzZOxdtVaKmXxxLUNQp4XJMoClivt9buvne-k67B32R_KjXBJoKGUYK5jc9PkPnXbAYsxbHh-SfX62hP1KBg9BW7f84hjqR_66LNw6xf5vODICe0Eb-DFIFHmBAnpo13cxmkaWlxSOK0HcP1kEbNOZ3dQoY_62mP_SxNJWr6Ac3hEOu2Cg1X7DeEHQoCPrFIdHlufaplRNg6Jjyn6Jfqdh2PhovD0ppjfpdw_vSVmI71n-3aYfHc4pYjr5fO6loYX2cgeSZm4ufwRZbHcKCYmp8UXsdJlFMzhZaZduQ-Cf8WXUqF8T9pq1CPUful5mJPdpcrtNOWAX6MetqE31z49tv6ZlinVMf_L5o3TaK5giYzdvUoEV3k68H02noOqS9SSGsYjOr3P6pcrgCt2uVAkautGNQ_zNfbjn8r8tK3Ee8aiDaxBdcHqBVpV3HNQHuw3S7NFI76NgjO8ZygEV-pza7xm_7KVvROG5h6Kkc1VClCcFLmxEo049XXIao6G8__V4uccPG1RneWN0CwixG2y7rw8rIWePI9VDPerYYvYPOANCgwz3Sa195npVuotH7R3nA=s368-no?authuser=1",
+      isComplete: true,
+      isInTeam: false,
+    },
+    {
+      id: 9,
+      title: "Gorghitondi",
+      category: "Packaging Design",
+      year: 2020,
+      image:
+        "https://lh3.googleusercontent.com/AnF1mc-Hk6Us7p4wnV3dRUR3izE_1HeDFkhLKWLwAxe8dPTRHlyApG0y0vTNW3b1-Ao3CwGRPilBFmc9zzD3J-sZjOnWFAYVJ8hbD81Yt3XZ7BeNJGIfRDZk7ulGF9gq-Xuwte-5QeBQxAAJOhXHOGWZKfPDchvnasvDX45l8mm2hZ5gweWNE-dACE27nH3ydLOlXatJJqhgHUAsn-l8F07qsxM1egs0h_nPPbo-phT-idgd0Rw1PnhUdb6invTxIX3DzXQ52sSQJV1ixkSZNDizovfVSeYbheB44JKiVuHnCegMdUzH8KZtLgs2_zEL6vwNdDTuAhWQJyuQFBkygUCBMXfsLwC-s6cnG866CB1Z0Rqv8b-5UyqWwzmq86E2lzCf95SsCVEE2HbYIKazZroruOLClbSnFGVIySTLfrUq1gVU4U_B1RIEKWIqpUiwFo-PQZSqI5LeWHR3k9cAAgwv_KaixWkWvRoWPn4RK1C3Ec5RTSLaUSIw_l3A_EuRiaxwEfLKHwReG348OR8jeacOv8beli2Dub0ZRs2Wzucy5bqSekob61xMwoa8Qfa8sRlcaL1TlCA_5T0xg4z2DZjRACqFuvFs6tFcK3bug0bjXB001DoDHD6SrEC_QT9zgKwaDLsjB583LCWKoMiBGi5dhNaYUvCkwnDWlwqoqHw2WDYhNXSXbIS8rmjKqNlag19m1Zo7bTja2NClj8y_tSik1awGvo9vQgK4HZs3ccf_Whi9jtjxuhYrbUWfuQ4pU29jDhUKDrw9jObuYThq6Wh9gYbkH12iOa1o7tlc6fc8dL4zwUuvs4HI1B0WMON47NZWzwHVvn4SRSCUtUj8qUFTUy52vqhaAw1qG7gf2LKrZq5t5pJ1uQ5xYzzw15px8wk4mCVOIlcN65iGYAKwnmO0JcEVKTpRn2tzcwN5QVVlnffNJqz6yw-C7io-1BoZtPkMczZ-l5Jq1Z3CWfgm7-Zqq9ZiT-s9ONc_7H5fzOrXgZaKH_3vLbc_9UFR9uw0m5zLuD4gHsAO-SVOOvsSv-uwxA=w138-h276-no?authuser=1",
+      isComplete: true,
+      isInTeam: true,
+    },
+    {
+      id: 10,
+      title: "Didot: type specimen",
+      category: "Layout Design",
+      year: 2019,
+      image:
+        "https://lh3.googleusercontent.com/LW0fDnObuCyAvfsRGnBOfmdzXwCtACLUyluQwqHCKUeUYzVCO3d941M8b1TsbTwOZK1UEEiGzAgq9VC9i1Yn-RtWTKxXVg-MCov7dwn-YuXVsi035X7U97X-NXA_DXnGodGDkN8DXSuQw7OKushAJUijcP19eqcP6LNerRrprme4jO9n2XOIntUM2Wgi_tJS-X55UyU-9glHVoNORjMTaVnGemXMky1VqIsDr7AepQBRSYYtEzC4YVCPzZ4suVyFCv1CKjKuMG371aq7E9CWiYJKOrmV4YH_9yEuDEt8Gr_4NxiCx4Jorr_qMxQb7z6uxP-0_dvks8jDlASm6hb98PC9MKp17K7jM51k0e6e6gTlLHRX3_B5u74gFw3YSW_J9NJOlxW20K0EoyuqmcYrZY5q6f9OHFUxImq7XvQcgp1r2uuLSAvU9GK5M0C_RU1kTe8uxgNV_Ko5jD_2BfMmSjXR_ZHHdyNdAGiqYLwrX-Scm4zFdRKCCJLp-IEvSKAmMgjCAzC-ITthKdYobe45BGpJsqzdGq6Y_e7Y2a_FK-57WgoJa8514cTnuYhXBUoCu0GAm4LCrHVmILkSoCXHiVy14UhGwJttaedJaiTDPfFjfku9kjuFW8aoadHN7BUf1t5MFKgKj9hz2WiAooAtJgFAWMuceefQ2AOxhw66gTu2fp6XeNOaOytlWunfEpCyW8uxWYHEyqbl13vh0_IALW0gUyc6-6JQ4rNZ9O0gdlolMkAAFp98qqhFY0Nl0GNWVdduf-P9aUrUQ5XKdXyBGNDWiYcbO-4rA541Tjmg2Rey54fnNgwWh0jTY4vgOAr_cFXZYosOHcgYjTB37_7lUdGRrqndzAuOYdnZtihj810LV5Fsug9-sv8-tMbtisqBld-UIMXEv89887mbTUA1OA78b7tdhKpYbmVb3t6J7-ohhFemK3qHgACcAgyWrmE6dR7Yhh9HX9j24E8NmHNPnzqNoYAm0eqwEO4_MOPhGB6cv0rrI0gZEz5MXRGQ-llEqyqdXIrNbJdL03OKyJsIKRBLYQ=w1211-h648-no?authuser=1",
+      isComplete: true,
+      isInTeam: false,
+    },
+    {
+      id: 11,
+      title: "Didot: type specimen",
+      category: "Layout Design",
+      year: 2019,
+      image:
+        "https://lh3.googleusercontent.com/ocyh-XdUWIYKrxT26kC_UN4bGU3LWVRa1c2ekvYPVxiSo5mDhJlyxd396g2kwOQ4tb0Qz4U3YIOr_aAe6-B3doGJ91Bq22l2d5gQAu42JtU-UwvGh12Ivd1dpPxuT3btpl0aPvTBYXmucGfCQQIbktxdeHIO4NU_P2f12gtQYpoC5qnXG9lN8fgZogNScQQa0opMjI8R-LpWBqWQcQftukEHnzmoNraKrOQc8gDnqX5g-034Hrj15Xf6mGdA2-RVRa9EgfMappohlz5aTmYpjFkHZehTkCbt4v1qlWvUpQm3euxHLH6nJcZypZ08OfXbo45lBcEWPY7_bCmtMoNK-XoABelDbt7TJaHLP-n22swTN8G7FYryTeUJqjrUiJcjTc2WYmN2GP8FC5zPwxRioGO7I9Wj8yNO0JovKmffi_NfwLU2ASzTGUkyyIZAiEZrrC-pO3-25KVAg7EHZENp0qNsofMqPchUvhJwUfA2LcRtmhjUVdvPvIR9LtQr9rLdxEtVv5sUfXU4-Np82kXNzDAdZE-ns9JVD6QkFXcYgEsBeHx9x_LvoqyyXvnVB1dkdN1RMRgjs9uwOwtyRv-hSGjeOWHlYSuQbtmDvgxSXCTOqdE_l4tOXKx7AIixRih5nJsIRKBcKlZAA9xry0SW9HADtUcgt8963rpCt7QatFbWpxDktmIu3suJMaInQx5RuKtMf57GMvZ-DeH1IauESWC9_uJetF_h70LlRnmWkHaFTWqfAaBGb83fbERzEntzfZXzY5ZKMNeWgXFkrTXKC0jJ6j_nY_ME8wvAerHXdUI-Y5wLmn7KTpLlorEUO0Sh6jcWT7sesHDxXfj7IRqWDlWl8YRWXMedPzibriglmPKu4dZc69teqFe-g_qXe4NZpCL7zu0egn2oYf9QSeOumYssgws50I0gBH3zb8WfAsSVeWLWMP-MiISsbyBXPyV-nh7JY3Mg2k0duDms5sRnglMxyJ9ZCK4DOvPaLHfU_I4NFTwURtMORiHkw9yx2JtQ6uB1cIq-Cm_z76ZbaEgkRkV9GQ=w1208-h648-no?authuser=1",
+      isComplete: true,
+      isInTeam: false,
+    },
+  ];
+  
+  export default projects;
+  
---
+++ 03-11-22 - FINAL/script.js
@@ -0,0 +1,109 @@
+import projects from "./projects.js";
+console.log(projects);
+
+const bodySection = document.querySelector("body");
+const sectionProjects = document.querySelector("#projects");
+const sectionProjectsTitle = document.querySelector(".projects-section__title");
+const projectsGallery = document.querySelector(".projects-gallery");
+
+const brandCategory = document.querySelector(".brand-design");
+const uxuiCategory = document.querySelector(".uxui-design");
+const packCategory = document.querySelector(".packaging-design");
+const layoutCategory = document.querySelector(".layout-design");
+
+const createCard = (title, category, imgUrl, appendTo) => {
+  const cardEl = document.createElement("div");
+  cardEl.className = "card";
+
+  const titleEl = document.createElement("h4");
+  titleEl.className = "card-title";
+  titleEl.textContent = title;
+
+  const CategoryEl = document.createElement("div");
+  CategoryEl.textContent = category;
+
+  const imageEl = document.createElement("img");
+  imageEl.setAttribute("src", imgUrl);
+  imageEl.setAttribute("alt", category);
+
+  cardEl.append(titleEl, imageEl);
+  appendTo.appendChild(cardEl);
+};
+
+// Filtro le card per categoria - Brand Design
+projects
+  .filter((project) => project.category === "Brand Design")
+  .map((project) => {
+    createCard(project.title, project.category, project.image, brandCategory);
+  });
+
+// Filtro le card per categoria - UX/UI Design
+projects
+  .filter((project) => project.category === "UX/UI Design")
+  .map((project) => {
+    createCard(project.title, project.category, project.image, uxuiCategory);
+  });
+
+// Filtro le card per categoria - Packaging Design
+projects
+  .filter((project) => project.category === "Packaging Design")
+  .map((project) => {
+    createCard(project.title, project.category, project.image, packCategory);
+  });
+
+// Filtro le card per categoria - Layout Design
+projects
+  .filter((project) => project.category === "Layout Design")
+  .map((project) => {
+    createCard(project.title, project.category, project.image, layoutCategory);
+  });
+
+// ----------------------------------------
+
+const $qs = (el) => document.querySelector(el);
+
+const heroPic = $qs(".hero-img__pic2");
+const showPic = () => (heroPic.style = "opacity: 1; transition: linear 1s;");
+const timeoutShowPic = setTimeout(showPic, 1000);
+
+const heroRect = $qs(".hero-img__rect2");
+const showRect = () =>
+  (heroRect.style =
+    "opacity: 1; transition: linear 1s; transform: translateY(100px);");
+const timeoutShowRect = setTimeout(showRect, 1400);
+
+const heroCircle = $qs(".hero-img__circle2");
+const showCircle = () =>
+  (heroCircle.style =
+    "opacity: 1; transition: linear 1s; transform: translate(300px, -350px);");
+const timeoutShowCircle = setTimeout(showCircle, 1500);
+
+const heroTitle = $qs(".hero-text__title2");
+const showTitle = () =>
+  (heroTitle.style = "opacity: 1; transition: linear 1s;");
+const timeoutShowTitle = setTimeout(showTitle, 1800);
+
+const heroPar = $qs(".hero-text__par2");
+const showPar = () => (heroPar.style = "opacity: 1; transition: linear 1s;");
+const timeoutShowPar = setTimeout(showPar, 2700);
+
+const heroDescr = $qs(".hero-text__descr");
+const showDescr = () =>
+  (heroDescr.style = "opacity: 1; transition: linear 1s;");
+const timeoutShowDescr = setTimeout(showDescr, 3000);
+
+let modalBtnEl = $qs(".open-modal");
+const showModalBtn = () =>
+  (modalBtnEl.style = "opacity: 1; transition: linear 1s;");
+const timeoutShowModalBtn = setTimeout(showModalBtn, 3500);
+
+const modalEl = $qs(".modal");
+
+modalBtnEl.addEventListener("click", () => {
+  modalEl.style = "visibility: visible;";
+});
+
+const closeModalBtnEl = $qs(".close-modal");
+closeModalBtnEl.addEventListener("click", () => {
+  modalEl.style = "opacity: 0;";
+});
