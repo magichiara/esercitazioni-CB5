88c05f9d44e1ac4ea5649551b626c0f151be7258
magic_c <info.chiaramaggio@gmail.com>
2023-02-01 19:15:17 +0100

    correzione

--- next-project/src/layouts/MainLayout.jsx
+++ next-project/src/layouts/MainLayout.jsx
@@ -1,14 +1,12 @@
 import styles from "./styles.module.scss";
 import Navbar from "@/components/navbar/Navbar";
 import { Fragment } from "react";
-import Footer from "@/components/footer/Footer";
 
 const MainLayout = ({ children }) => {
   return (
     <Fragment>
       <Navbar />
       {children}
-      <Footer />
     </Fragment>
   );
 };
